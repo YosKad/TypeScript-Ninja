b2e7bde9179f1899f2947f169bc01de545ba9331
YosKad <yoskadtl@gmail.com>
2023-05-01 14:05:05 +0300

    first commit

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+// array 
+var names = ['liness', 'yoskad', 'sapir'];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var mixed = ['yos', 4, false];
+mixed.push(4);
+mixed.push('yos');
+mixed.push(true);
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+// array 
+
+let names = ['liness' , 'yoskad' , 'sapir'];
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let mixed = ['yos' , 4 , false];
+
+mixed.push(4);
+mixed.push('yos');
+mixed.push(true);
+
